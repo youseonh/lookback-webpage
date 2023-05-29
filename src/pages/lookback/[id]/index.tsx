7203af1c559cf9d24ecbcae55bce95d4ad699983
youseon <dbtjs0221@gmail.com>
2023-05-30 00:57:20 +0900

    feat: timestamp 날짜 포맷해서 표시하기 추가 #8

--- src/pages/lookback/[id]/index.tsx
+++ src/pages/lookback/[id]/index.tsx
@@ -5,25 +5,27 @@ import { useGetWayBack } from "@/src/apis/useWayBack";
 import { useRouter } from "next/router";
 import type { ColumnsType } from "antd/es/table";
 import { useEffect, useState } from "react";
-import useLocalStorage from "@src/hooks/useLocalStorage";
+import { localStorageAtom } from "@/src/states";
+import { useRecoilValue } from "recoil";
+import dayjs from "dayjs";
 
 type AllowedKeys = "key" | "urlkey" | "mimetype" | "statusCode" | "original" | "timestamp" | "digest" | "length";
 
 type DataType = {
   [key in AllowedKeys]: string | number | React.Key;
 };
-interface ILocalStorage {
-  name: string;
-  url: string;
-}
+/**
+ * 상세 보기 페이지
+ */
 const Detail = () => {
-  const [storedValue, setValue] = useLocalStorage<ILocalStorage[]>("look-web", []);
   const [currentUrl, setCurrentUrl] = useState<string>("");
   const [columns, setColumns] = useState<ColumnsType<DataType>>([]);
   const [tableData, setTableData] = useState<DataType[]>([]);
+  const localValues = useRecoilValue(localStorageAtom);
+
   const router = useRouter();
   const TIME = 1000 * 60 * 5; // 5m
-  const { data, loading } = useGetWayBack(currentUrl, {
+  const { data } = useGetWayBack(currentUrl, {
     staleTime: TIME,
     cacheTime: TIME,
   });
@@ -32,14 +34,18 @@ const Detail = () => {
     setTableData([]);
   };
 
-  useEffect(() => {
-    // 현재 url 체크
-    for (const value of storedValue) {
+  // 현재 url 체크
+  const checkCurrentUrl = () => {
+    for (const value of localValues) {
       const { name, url } = value;
-      if (name === router.query.id) {
+      if (name === router.query.id && url) {
         setCurrentUrl(url);
       }
     }
+  };
+
+  useEffect(() => {
+    checkCurrentUrl();
     // 초기화
     init();
     // 받아온 데이터가 있으면
@@ -51,20 +57,17 @@ const Detail = () => {
         }),
       );
 
-      data.forEach((d: DataType) => {
-        const ac = tempColumns.reduce((acc, curr: string, idx: AllowedKeys) => {
-          return { ...acc, [curr]: d[idx] };
+      data.forEach((dataObj: DataType) => {
+        const reducedDatas = tempColumns.reduce((acc, curr: string, idx: AllowedKeys) => {
+          if (curr === "timestamp") {
+            return { ...acc, [curr]: dayjs(dataObj[idx]).format("YYYY.MM.DD HH:mm:ss") };
+          }
+          return { ...acc, [curr]: dataObj[idx] };
         }, new Object());
         setTableData((prev) => {
-          return [...prev, ...[ac]];
+          return [...prev, ...[reducedDatas]];
         });
       });
-
-      // const result = tempColumns.reduce((acc, curr, idx) => {
-      //   for (let i = 1; i < data.length; i++) {
-      //     return { ...acc, [curr]: data[i][idx] };
-      //   }
-      // }, []);
     }
   }, [data]);
 
